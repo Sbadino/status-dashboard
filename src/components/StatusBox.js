cb7b62fe4685fd0a64e94ea390852e2c5f8e099a
Sbadino <badino.santi@gmail.com>
2022-08-09 22:58:55 -0300

    Accounts

--- src/components/StatusBox.js
+++ src/components/StatusBox.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function StatusBox() {
+  const [fetchAccountsData, setFetchAccountsData] = useState(null);
+  console.log(fetchAccountsData);
+
+  useEffect( () => {
+    setInterval(() => {
+      fetch("https://status-dashboard-api.herokuapp.com/accounts")
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setFetchAccountsData(data);
+      }) 
+    }, 15000)
+  }, [])
+
   return (
     <Container>
       <Row>
-        <Col sm>
+        {
+          fetchAccountsData && 
+          <Col sm>
           <Card className="mt-5" style={{ width: '18rem', height: '13rem' }}>
             <Card.Body>
               <Card.Title>
                 ACCOUNTS
               </Card.Title>
-              <Card.Subtitle className="pt-1 pb-1" style={{ backgroundColor: "#32CD32", color: 'white' }}>
-                Healthy
+              <Card.Subtitle className="pt-1 pb-1" style={{ backgroundColor: fetchAccountsData.success ? "#32CD32" : "#FF0000", color: 'white' }}>
+                {fetchAccountsData.success ? "Healthy" : "Error"}
               </Card.Subtitle>
               <Card.Text>
                 accounts-1234
@@ -26,28 +43,8 @@ function StatusBox() {
             </Card.Body>
           </Card>
         </Col>
+        }
 
-        <Col sm>
-          <Card className="mt-5" style={{ width: '18rem', height: '13rem'}}>
-            <Card.Body>
-              <Card.Title>
-                INVITES
-              </Card.Title>
-              <Card.Subtitle className="pt-1 pb-1" style={{ backgroundColor: "#FF0000", color: 'white' }}>
-                Error
-              </Card.Subtitle>
-              <Card.Text style={{ color: '#FF0000' }}>
-                OUTAGE
-              </Card.Text>
-              <Card.Text style={{ color: '#FF0000' }}>
-                403
-              </Card.Text>
-              <Card.Text style={{ color: '#FF0000' }}>
-                Forbidden
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
       </Row>
     </Container>
   );
